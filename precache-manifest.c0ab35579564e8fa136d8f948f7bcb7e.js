8912fea7fe315a9377ceb2a551cc6263ac3da40c
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-10-04 00:37:38 +0530

    Updates

---
+++ precache-manifest.c0ab35579564e8fa136d8f948f7bcb7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cc5190c8688fce874b555fac142d46",
+    "url": "/othello-board/index.html"
+  },
+  {
+    "revision": "8431e037bdfd3e54bdae",
+    "url": "/othello-board/static/css/main.321d4173.chunk.css"
+  },
+  {
+    "revision": "c5b8e80a2d418c373f41",
+    "url": "/othello-board/static/js/2.fc693b72.chunk.js"
+  },
+  {
+    "revision": "8431e037bdfd3e54bdae",
+    "url": "/othello-board/static/js/main.6f08c6f9.chunk.js"
+  },
+  {
+    "revision": "2bd1b56815048e68e2da",
+    "url": "/othello-board/static/js/runtime-main.67766b90.js"
+  }
+]);

8bbb38d6fc1696a42dcd6613669159b062217027
Chandan Kumar <cksharma122@gmail.com>
2019-11-03 23:56:58 +0530

    Adding PWA functionality

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Game from "./components/Game/Game";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+serviceWorker.register();
